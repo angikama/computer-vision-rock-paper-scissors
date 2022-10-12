a60fe5148b54e2afa7b3f03bc140d15ba992f1ec
angikama <angela.akamara@gmail.com>
2022-10-12 13:41:23 +0100

    Uses integers as input

--- manual_rps.py
+++ manual_rps.py
@@ -1,72 +1,83 @@
 import random
 
 class RPS():
-
-    def __init__(self):
-        self.actions = ["R","P","S"]
+    user_wins = 0
+    cpu_wins = 0
 
 # randomly choses an action for the computer      
-    def get_computer_choice(self):
-        self.computer_choice = random.choice(self.actions)
-        if self.computer_choice == "R":
-            self.computer_choice_name = "Rock"
-        elif self.computer_choice == "P":
-            self.computer_choice_name = "Paper"
+    def get_computer_choice(computer_choice):
+        computer_choice = random.randint(1,3)
+        if computer_choice == 1:
+            computer_choice = "Rock"
+        elif computer_choice == 2:
+            computer_choice = "Paper"
         else:
-            self.computer_choice == "S"
-            self.computer_choice_name = "Scissors"
-
-        return self.computer_choice
-
-# gets input from the user
-    def get_user_choice(self):
-        self.user_choice = input("Do you choose Rock(R), Paper(P) or Scissors(S)? ").upper()
-        if self.user_choice == "R":
-            self.user_choice_name = "Rock"
-        elif self.user_choice == "P":
-            self.user_choice_name = "Paper"
-        else:
-            self.user_choice == "S"
-            self.user_choice_name = "Scissors"
-        return self.user_choice
-
-# decides who wins
-    def get_winner(self,computer_choice, user_choice, computer_choice_name, user_choice_name):
-        print(f"You chose {user_choice_name} and the Computer chose {computer_choice_name}")
-        while True:
-            if computer_choice == user_choice:
-                 print ("You Draw!")
-            elif computer_choice == "R" and user_choice == "P":
-                print("You win!")
-            elif computer_choice == "R" and user_choice == "S":
-                print("The computer wins!")
-            elif computer_choice == "P" and user_choice == "R":
-                print("The computer wins")
-            elif computer_choice == "P" and user_choice == "S":
-                print("You win!")
-            elif computer_choice == "S" and user_choice == "R":
-                    print("You win!")
-            else:
-                computer_choice == "S" and user_choice == "P"
-                print("The computer wins!")
-                
-            play_again = input("Do you want to play again? (y/n) : ")
-            if play_again.lower() == "y":
-                play()
-            else: 
-                play_again.lower() == "n"
-                break
-                
+            computer_choice == 3
+            computer_choice = "Scissors"
+        return computer_choice
         
 
+# gets input from the user using intergers
+    def get_user_choice(user_choice):
+        user_choice = input("Do you choose (Rock[1], Paper[2] or Scissors[3]?: ")
+        user_choice = int(user_choice)
+
+        if user_choice == 1:
+            user_choice = "Rock"
+        elif user_choice == 2:
+            user_choice = "Paper"
+        else:
+            user_choice == 3
+            user_choice = "Scissors"
+        return user_choice
+
+
+# decides who wins
+    def get_winner(self,computer_choice, user_choice):
+        print()
+        print(f"You chose {user_choice} and the Computer chose {computer_choice}")
+        print()
+        
+
+        while True:
+            if computer_choice == user_choice:
+                print ("You Draw!")
+            
+            elif computer_choice == 1:
+                if user_choice == 2:
+                    print("You win!")     
+                else:
+                    print("The computer wins!")
+                           
+            elif computer_choice == 2:
+                if user_choice == 1:
+                    print("The computer wins")   
+                else:
+                    print("You win!")
+                
+            elif computer_choice == 3:
+                if user_choice == 1:
+                    print("You win!")
+                    user_wins+=1
+                else:
+                    print("The computer wins!")
+                    
+    
+            play_again = input("Do you want to play again? (y/n) : ").lower()
+            if play_again == "n":
+                break
+            else:
+                play()
+
+    
+             
+# allows the whole game to be played
 def play():
     game = RPS()
     computer_choice = game.get_computer_choice()
     user_choice = game.get_user_choice()
-    computer_choice_name = game.computer_choice_name
-    user_choice_name = game.user_choice_name
-    game.get_winner(computer_choice, user_choice, computer_choice_name, user_choice_name)
-    
+    game.get_winner(computer_choice, user_choice)
+
 
   
 play()
